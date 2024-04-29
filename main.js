08c42296b776f56bff1e9f1f5aa660b61c9d3e4f
abidakosar <161754486+abidakosar@users.noreply.github.com>
2024-04-29 02:37:45 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+"use strict";
+//SLICE METHOD
+// let array = ["haram","abrish","habib","anum"]
+// let newarray = array.slice(1,3)
+// console.log(newarray)
+//SPLICE METHOD
+// let array = ["haram","abrish","habib"]
+// array.splice(1,2,"anum")
+// console.log(array)
+// for(let i=1;i<10;i++){
+// console.log(i)
+// }
+// for (let i=1; i<=10;i++){
+// let numb = 2;
+// let ans = 2*i
+// console.log(numb+" x "+i+" = "+ ans)
+// }
+// for (let i=1; i<=10; i++){
+// let numb = 2;
+// let ans = 2*i
+// console.log(numb + " x " +  i + " = " + ans)
+// }
+// for (let i=1;i<=10;i++){
+// }
+// for(let i=1;i<=10;i++){
+// let numb = 5;
+// let ans = 5*i
+// console.log(numb+" X "+i+" = "+ a
+// for(let i=1;i<=10;i++){
+// let numb=3
+// let ans = 3*i
+// console.log(numb+" x "+i+" = "+ans)
+// }
+// let arr = ["haram","abrish","anum","rida","tanveer","abida","habib"]
+// for(let i=0;i<=6;i++){
+// console.log(arr[i])
+// }
+// let sub = ["eng","urdu","draw","s,st","sindhi"]
+// for(let i=0;i<=4;i++){
+// console.log(sub[i])
+// }
